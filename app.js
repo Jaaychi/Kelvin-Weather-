6e41339964614bc76ec927267e5467c4f11a1f5e
Jeffrey Chi <thejeffreychi@gmail.com>
2022-04-11 22:23:25 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// today's forecast is 293 Kelvin
+const kelvin =  0;
+
+// turning Kelvin into celsius
+let celsius = kelvin - 273;
+
+// turning celsius into fahrenheit
+var fahrenheit = celsius * (9/5) + 32;
+
+console.log(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
